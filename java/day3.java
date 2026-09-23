eb2e904ffff04ac313a4494c26bdb75db0ac6555
sahan24cs121 <sahana.24cs121@sode-edu.in>
2026-09-23 22:20:29 +0530

    HackerRank - 4 Java problems

---
+++ java/day3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class day3 {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        if(N%2!=0){
+            System.out.println("Weird");
+        }
+        else if(N>=2 && N<=5){
+            System.out.println("Not Weird");
+        }
+        else if(N>=6 && N<=20){
+            System.out.println("Weird");
+        }
+        else 
+            System.out.println("Not Weird");
+        }
+}

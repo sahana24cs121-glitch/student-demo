e667f640005e431293d0e4d2d08f346a9ece3f7b
sahan24cs121 <sahana.24cs121@sode-edu.in>
2026-09-23 22:43:26 +0530

    Update HackerRank Day 6

--- java/day6.java
+++ java/day6.java
@@ -1,3 +1,14 @@
+import java.util.*;
 public class day6 {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(N + " x " + i + " = " + (N * i));
+        }
+        
+        sc.close();
+}
     
 }
+

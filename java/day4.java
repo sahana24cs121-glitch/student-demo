35325e3d2100e93ab8c6aa673a6d09ee3fccd7dd
sahan24cs121 <sahana.24cs121@sode-edu.in>
2026-09-23 22:29:35 +0530

    Update HackerRank Day 4

--- java/day4.java
+++ java/day4.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class day4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i=sc.nextInt();
+        double d=sc.nextDouble();
+        sc.nextLine(); // Consume the newline character
+        String s=sc.nextLine();
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(s);
+    }
+    }
+
     
-}
+

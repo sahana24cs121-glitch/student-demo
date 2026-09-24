0429f9815e90b1949f1c700a851d874435313dc5
sahan24cs121 <sahana.24cs121@sode-edu.in>
2026-09-24 23:44:27 +0530

    Add search an element program

---
+++ java/01-Array/SearchAnElement.java
@@ -0,0 +1,23 @@
+public class SearchAnElement {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+        int search = 30;
+
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == search) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Element found");
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}

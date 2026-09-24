5fc33f77fdf853a389d44ce1ee2c5e9c1c0dda22
sahan24cs121 <sahana.24cs121@sode-edu.in>
2026-09-24 23:57:58 +0530

    Add maximum subarray sum program

---
+++ java/01-Array/MaximumSubarraySum.java
@@ -0,0 +1,24 @@
+public class MaximumSubarraySum {
+    public static void main(String[] args) {
+
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        int sum = 0;
+        int max = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+
+            sum = sum + arr[i];
+
+            if (sum > max) {
+                max = sum;
+            }
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        System.out.println("Maximum subarray sum = " + max);
+    }
+}

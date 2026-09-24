1be35b41cb4f3541f44bca8376d6fdebaf4b38c2
sahan24cs121 <sahana.24cs121@sode-edu.in>
2026-09-24 23:29:09 +0530

    Add average of array program

---
+++ java/01-Array/Main.java
@@ -0,0 +1,16 @@
+public class average {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40};
+
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+
+        double average = (double) sum / arr.length;
+
+        System.out.println("Average = " + average);
+    }
+}

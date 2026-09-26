a47102332be1f5501b9cf852496a191b5c146c00
sahan24cs121 <sahana.24cs121@sode-edu.in>
2026-09-26 23:22:46 +0530

    Add linear search program

---
+++ java/03-Searching/LinearSearch.java
@@ -0,0 +1,1 @@
+public class LinearSearch { public static void main(String[] args) { int[] arr = {10,20,30,40,50}; int target = 40; int index = -1; for (int i = 0; i < arr.length; i++) { if (arr[i] == target) { index = i; break; } } if (index != -1) { System.out.println("Element found at index " + index); } else { System.out.println("Element not found"); } } }

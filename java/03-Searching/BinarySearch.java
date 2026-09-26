8cc2b1c5700f8c810cbe511aedfc4973f7ea8f1e
sahan24cs121 <sahana.24cs121@sode-edu.in>
2026-09-26 23:30:27 +0530

    Add binary search program

---
+++ java/03-Searching/BinarySearch.java
@@ -0,0 +1,1 @@
+public class BinarySearch { public static void main(String[] args) { int[] arr = {10,20,30,40,50,60,70}; int target = 60; int low = 0; int high = arr.length - 1; while (low <= high) { int mid = (low + high) / 2; if (arr[mid] == target) { System.out.println("Element found at index " + mid); break; } else if (arr[mid] < target) { low = mid + 1; } else { high = mid - 1; } } } }
